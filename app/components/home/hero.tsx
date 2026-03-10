a4af798077e68ead01c8f116ff409f6c302fc346
Suraj <kumarsj420@gmail.com>
2026-03-10 10:59:30 +0530

    fix deployment

--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+// hero section 
+
 import { useState, useEffect, useRef } from 'react'
 import { motion, AnimatePresence, useScroll, useTransform } from 'framer-motion'
 import Button from '../ui/Button';
