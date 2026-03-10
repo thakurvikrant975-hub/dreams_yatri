4af5e055207572897c895ffdba90574520831a35
Vikrant <thakurvikrant975@gmail.com>
2026-03-10 11:18:32 +0530

    vercel fix

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,12 +1,23 @@
 export const revalidate = 1800 // ISR every 30 minutes
 import Hero from "../components/home/hero"
 
+//vercel fix
+
 export default function Home() {
 
   return (
     <main>
       <Hero />
       <ul>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
         <li>*</li>
         <li>*</li>
         <li>*</li>
