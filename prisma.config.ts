75f8800acb75f79c6596cb93e5e2093a3918f085
Vikrant <thakurvikrant975@gmail.com>
2026-03-28 18:24:15 +0530

    fix vercel deployment

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,7 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
@@ -11,4 +12,5 @@ export default defineConfig({
   datasource: {
     url: process.env["DATABASE_URL"],
   },
+  
 });
